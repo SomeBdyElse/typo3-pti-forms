SomeBdyElse/typo3-pti-forms

commit 1a8390f6f1d8ce634c81959cde6dd3d913d6614c

[TASK] Migrate to TYPO3 13.4
* Use correct hashing for TYPO3 v13
* Drop TYPO3 v12 support and require TYPO3 v13.4

Classes/Form.php
@@ -2,10 +2,11 @@
 
 namespace PrototypeIntegration\Forms;
 
+use TYPO3\CMS\Core\Crypto\HashService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\MvcPropertyMappingConfigurationService;
 use TYPO3\CMS\Extbase\Mvc\RequestInterface;
-use TYPO3\CMS\Extbase\Security\Cryptography\HashService;
+use TYPO3\CMS\Extbase\Security\HashScope;
 use TYPO3\CMS\Extbase\Service\ExtensionService;
 
 class Form
@@ -127,7 +128,6 @@ class Form
      */
     protected function renderTrustedPropertiesField(): array
     {
-        /** @var Field $trustedPropertiesField */
         $trustedPropertiesField = $this->createPlainHiddenField('__trustedProperties');
         $trustedPropertiesField->setValue($this->mvcPropertyMappingConfigurationService->generateTrustedPropertiesToken(
             $this->getFormFieldNames(),
@@ -168,12 +168,12 @@ class Form
             ->render();
 
         $hiddenFields[] = $this->createPlainHiddenField('__referrer[arguments]')
-            ->setValue($this->hashService->appendHmac(base64_encode(serialize($request->getArguments()))))
+            ->setValue($this->hashService->appendHmac(base64_encode(serialize($request->getArguments())), HashScope::ReferringArguments->prefix()))
             ->setRespectSubmittedDataValue(false)
             ->render();
 
         $hiddenFields[] = $this->createPlainHiddenField('__referrer[@request]')
-            ->setValue($this->hashService->appendHmac(serialize($actionRequest)))
+            ->setValue($this->hashService->appendHmac(json_encode($actionRequest), HashScope::ReferringRequest->prefix()))
             ->setRespectSubmittedDataValue(false)
             ->render();
 
@@ -185,7 +185,9 @@ class Form
         $fieldNames = [];
         /** @var Field $field */
         foreach ($this->fields as $field) {
-            $fieldNames[] = $field->renderName();
+            foreach ($field->tokenNames() as $tokenName) {
+                $fieldNames[] = $tokenName;
+            }
         }
 
         return $fieldNames;

commit 83433159101cf0029ab8c3254d84b21d3b04fa1f

[BUGFIX] Fix invalid method call during rendering
This fixes a bug introduced in 1a8390f, as a partial feature to allow
multiple field names per field.

Classes/Form.php
@@ -185,9 +185,7 @@ class Form
         $fieldNames = [];
         /** @var Field $field */
         foreach ($this->fields as $field) {
-            foreach ($field->tokenNames() as $tokenName) {
-                $fieldNames[] = $tokenName;
-            }
+            $fieldNames[] = $field->renderName();
         }
 
         return $fieldNames;